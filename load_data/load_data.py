7b8f71ef8cc05f5256ad6d86c101dafd2aa70570
ovechkinVT <vsevolod.ovechkin95@gmail.com>
2019-10-01 22:26:39 +0300

    add load_data dir

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -3,10 +3,10 @@ import json
 from tqdm import tqdm
 
 # load photo links
-with open("/home/ubuntu/clothes/data/street2shop/photos.txt") as f:
+with open("/home/ubuntu/data/street2shop/photos.txt") as f:
     img_links = f.readlines()
 
-with open("/home/ubuntu/clothes/data/street2shop/useful_img_id.json", "r") as f:
+with open("/home/ubuntu/clothes/street2shop/load_data/useful_photo_ids.json", "r") as f:
     useful_img_id = set(json.load(f)) # list of images' ids
 
 # get id and link
@@ -16,9 +16,9 @@ img_links = map(lambda row: row.split(","),  img_links)
 img_links = filter(lambda x: int(x[0]) in useful_img_id, img_links)
 
 # only links with jpg endings (~90%)
-img_links = list(filter(lambda x: x[1].split(".")[-1][:3] == "jpg", img_links))
+img_links = list(filter(lambda x: x[1].split(".")[-1].startswith("jpg"), img_links))
 
-os.chdir("/home/ubuntu/clothes/data/street2shop/photos")
+os.chdir("/home/ubuntu/data/street2shop/photos")
 
 
 for img_id, link in tqdm(img_links, total=len(img_links)):
